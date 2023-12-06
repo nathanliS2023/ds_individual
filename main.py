1da92bd1339bd084ceac89c08913c04f6dd87649
Nathan <nathanli@andrew.cmu.edu>
2023-12-06 00:25:26 -0800

    added classification models

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from utils import load_and_preprocess_data, evaluate_function, hyperparameter_tuning
+from utils import load_and_preprocess_data, evaluate_regression, evaluate_classification, hyperparameter_tuning
 
 
 def print_top_n_defect_predictions(model, x_test, n):
@@ -23,35 +23,56 @@ def print_top_n_defect_predictions(model, x_test, n):
         print("Model does not support probability predictions.")
 
 
-def main():
-    x_train, x_test, y_train, y_test = load_and_preprocess_data("data/jm1.arff.txt")
+def evaluate_models(models, x_train, x_test, y_train, y_test, model_type):
+    scoring = None
+    evaluate = None
+    if model_type == 'regression':
+        scoring = "neg_mean_squared_error"
+        evaluate = evaluate_regression
+    elif model_type == 'classification':
+        scoring = "accuracy"
+        evaluate = evaluate_classification
 
-    # Select the algorithm for hyperparameter tuning
-    algorithms = [
-        'decision_tree',
-        'random_forest',
-        'svm',
-        'linear_regression'
-    ]
-
-    for algorithm_name in algorithms:
-        print(f"Processing {algorithm_name}...")
+    for model in models:
+        print(f"Processing {model}...")
 
         # Perform hyperparameter tuning
-        best_model = hyperparameter_tuning(x_train, y_train, algorithm_name)
+        best_model = hyperparameter_tuning(x_train, y_train, model, model_type, scoring=scoring)
 
         # Evaluate the best model
-        evaluation_results = evaluate_function(best_model, x_train, x_test, y_train, y_test, 0.515)
+        evaluation_results = evaluate(best_model, x_train, x_test, y_train, y_test)
 
         # Print evaluation results
-        print(f"Results for {algorithm_name}:")
+        print(f"Results for {model}:")
         for metric, value in evaluation_results.items():
             print(f"{metric}: {value}")
         print("\n")
 
         # Print top n samples most likely to have defects
-        print_top_n_defect_predictions(best_model, x_test, 10)
-        print("\n")
+        # print_top_n_defect_predictions(best_model, x_test, 10)
+        # print("\n")
+
+
+def main():
+    x_train, x_test, y_train, y_test = load_and_preprocess_data("data/jm1.arff.txt")
+
+    # Select the algorithm for hyperparameter tuning
+    regression_models = [
+        'decision_tree_reg',
+        'random_forest_reg',
+        'svr',
+        'linear_regression',
+    ]
+
+    classification_models = [
+        'decision_tree_clf',
+        'random_forest_clf',
+        'svc',
+        'logistic_regression'
+    ]
+
+    # Evaluate the models
+    evaluate_models(classification_models, x_train, x_test, y_train, y_test, model_type='classification')
 
 
 if __name__ == "__main__":
--- utils/train_model.py
+++ utils/train_model.py
@@ -1,22 +1,22 @@
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.svm import SVR, SVC
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
 import numpy as np
 
 # Dictionary mapping algorithms to their models and parameter grids
-algorithms = {
-    'decision_tree': {
+regression_algorithms = {
+    'decision_tree_reg': {
         'model': DecisionTreeRegressor(),
         'params': {
             'max_depth': [None, 10, 15, 20, 25],
             'min_samples_leaf': [1, 2, 5, 10]
         }
     },
-    'random_forest': {
+    'random_forest_reg': {
         'model': RandomForestRegressor(),
         'params': {
             'n_estimators': [10, 50, 100],
@@ -24,7 +24,7 @@ algorithms = {
             'min_samples_leaf': [1, 2, 5, 10]
         }
     },
-    'svm': {
+    'svr': {
         'model': SVR(),
         'params': {
             'C': [0.01, 0.1, 1, 10],
@@ -37,8 +37,44 @@ algorithms = {
     }
 }
 
+classification_algorithms = {
+    'decision_tree_clf': {
+        'model': DecisionTreeClassifier(),
+        'params': {
+            'max_depth': [None, 10, 15, 20, 25],
+            'min_samples_leaf': [1, 2, 5, 10]
+        }
+    },
+    'random_forest_clf': {
+        'model': RandomForestClassifier(),
+        'params': {
+            'n_estimators': [10, 50, 100],
+            'max_depth': [None, 10, 15, 20, 25],
+            'min_samples_leaf': [1, 2, 5, 10]
+        }
+    },
+    'svc': {
+        'model': SVC(),
+        'params': {
+            'C': [0.01, 0.1, 1, 10],
+            'gamma': ['scale', 'auto'],
+        }
+    },
+    'logistic_regression': {
+        'model': LogisticRegression(),
+        'params': {}
+    }
+}
 
-def hyperparameter_tuning(x_train, y_train, algorithm_name):
+
+def hyperparameter_tuning(x_train, y_train, algorithm_name, model_type, scoring="accuracy"):
+    if model_type == 'regression':
+        algorithms = regression_algorithms
+    elif model_type == 'classification':
+        algorithms = classification_algorithms
+    else:
+        raise ValueError("Invalid model type. Choose 'regression' or 'classification'.")
+
     if algorithm_name not in algorithms:
         raise ValueError("Invalid algorithm choice.")
 
@@ -47,13 +83,13 @@ def hyperparameter_tuning(x_train, y_train, algorithm_name):
     param_grid = algorithm['params']
 
     print("Doing grid search for", algorithm_name)
-    grid_search = RandomizedSearchCV(model, param_grid, n_iter=10, cv=5, scoring="neg_mean_squared_error", n_jobs=-1)
+    grid_search = RandomizedSearchCV(model, param_grid, n_iter=10, cv=5, scoring=scoring, n_jobs=-1)
     grid_search.fit(x_train, y_train)
     return grid_search.best_estimator_
 
 
 # Evaluate function for regression models with binary outcomes
-def evaluate_function(model, x_train, x_test, y_train, y_test, threshold=0.5):
+def evaluate_regression(model, x_train, x_test, y_train, y_test, threshold=0.5):
     # Predict continuous values
     train_predictions = model.predict(x_train)
     test_predictions = model.predict(x_test)
@@ -82,6 +118,32 @@ def evaluate_function(model, x_train, x_test, y_train, y_test, threshold=0.5):
 
     return {
         'train_mse': train_mse, 'test_mse': test_mse,
+        'train_accuracy': train_accuracy, 'test_accuracy': test_accuracy,
+        'train_precision': train_precision, 'test_precision': test_precision,
+        'train_recall': train_recall, 'test_recall': test_recall,
+        'train_f1': train_f1, 'test_f1': test_f1,
+        'test_confusion_matrix': test_confusion_matrix
+    }
+
+
+def evaluate_classification(model, x_train, x_test, y_train, y_test):
+    test_predictions = model.predict(x_test)
+    train_predictions = model.predict(x_train)
+
+    # Classification metrics
+    train_accuracy = accuracy_score(y_train, train_predictions)
+    test_accuracy = accuracy_score(y_test, test_predictions)
+    train_precision = precision_score(y_train, train_predictions)
+    test_precision = precision_score(y_test, test_predictions)
+    train_recall = recall_score(y_train, train_predictions)
+    test_recall = recall_score(y_test, test_predictions)
+    train_f1 = f1_score(y_train, train_predictions)
+    test_f1 = f1_score(y_test, test_predictions)
+
+    # Confusion matrix for test data
+    test_confusion_matrix = confusion_matrix(y_test, test_predictions)
+
+    return {
         'train_accuracy': train_accuracy, 'test_accuracy': test_accuracy,
         'train_precision': train_precision, 'test_precision': test_precision,
         'train_recall': train_recall, 'test_recall': test_recall,
